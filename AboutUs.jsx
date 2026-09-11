5961ebb0e5898aadde4c84062b9e13b4ee498982
rafaybaig1020 <rafaybaig499@gmail.com>
2026-09-11 19:43:14 +0500

    Add files via upload

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        We are a company providing quality products and services
+        to our customers.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
