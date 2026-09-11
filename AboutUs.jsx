df7b09b0ddcd156ba7a94362d4faf1bbb6a62c5a
rafaybaig1020 <rafaybaig499@gmail.com>
2026-09-11 19:49:30 +0500

    Add files via upload

---
+++ AboutUs.jsx
@@ -0,0 +1,27 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery is an online plant shop that provides
+        a variety of beautiful and healthy plants for your home
+        and garden.
+      </p>
+
+      <p>
+        Our goal is to make it easy for customers to discover
+        and purchase plants online. We offer quality plants at
+        affordable prices with a simple and user-friendly
+        shopping experience.
+      </p>
+
+      <p>
+        At Paradise Nursery, we believe that plants bring
+        beauty, freshness, and happiness to every space.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
